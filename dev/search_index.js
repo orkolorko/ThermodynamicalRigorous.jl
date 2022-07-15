bbd304d86495ff81fde49209c51049d8e15e00df
Documenter.jl <documenter@juliadocs.github.io>
2022-07-15 01:49:56 +0000

    build based on 58b1ba2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ThermodynamicalRigorous","category":"page"},{"location":"#ThermodynamicalRigorous","page":"Home","title":"ThermodynamicalRigorous","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ThermodynamicalRigorous.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ThermodynamicalRigorous]","category":"page"}]
+}
